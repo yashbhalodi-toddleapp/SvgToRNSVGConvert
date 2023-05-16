4db2784f5f17313cbab987f2d6bba4d8fd1b249c
Yash Bhalodi <yashbhalodi@toddleapp.com>
2023-05-16 20:57:27 +0530

    fix: index file generator script

--- generateIndexFile.sh
+++ generateIndexFile.sh
@@ -9,21 +9,21 @@ if [ -f "$index_file_path" ]; then
 fi
 
 # Loop through each file in the directory
+components=()
 for file in "${directory_path}"/*.js; do
   filename=$(basename "$file")
   component_name=$(echo "$filename" | sed 's/\.js$//')
-  
+
   # Append import statement to index file
   echo "import $component_name from './$filename';" >> "$index_file_path"
+  
+  components+=("$component_name")
 done
 
 # Append export statements to index file
 echo "" >> "$index_file_path"
 echo "export {" >> "$index_file_path"
-for file in "${directory_path}"/*.js; do
-  filename=$(basename "$file")
-  component_name=$(echo "$filename" | sed 's/\.js$//')
-  
+for component_name in "${components[@]}"; do
   echo "  $component_name," >> "$index_file_path"
 done
 echo "};" >> "$index_file_path"
