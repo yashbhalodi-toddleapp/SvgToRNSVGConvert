76da76a6a5a6b07a7913a5c9c3a945be874db795
Yash Bhalodi <yashbhalodi@toddleapp.com>
2023-05-18 17:28:46 +0530

    fix: minor script maintainance

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-import tmp from "./template.js";
-import tmpRegular from "./templateRegular.js";
-import coloredTmp from "./coloredTemplate.js";
+import boldTemplate from "./templates/boldTemplate.js";
+import regularTemplate from "./templates/regularTemplate.js";
+import coloredTemplate from "./templates/coloredTemplate.js";
+
 import { transform } from "@svgr/core";
 import path from "path";
 import { readFile, writeFile, readdir } from "fs/promises";
+
+const inputDir = "./input/Bold";
+const regularDir = "./input/Regular";
+const coloredDir = "./input/Colored";
+
+const outputDir = "./output";
 
 const config = {
   plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx", "@svgr/plugin-prettier"],
@@ -52,15 +59,22 @@ const getSvg = async (svgPath) => {
   }
 };
 
-const generateJSForSvgFile = async (componentName, svgFile, regularDir) => {
-  const svgCode = await getSvg(svgFile);
+const generateJSForSvgFile = async ({
+  componentName,
+  svgFilePath,
+  template,
+}) => {
+  const svgCode = await getSvg(svgFilePath);
 
-  const isColored = svgFile.includes("Colored");
+  const isColored = svgFilePath.includes("Colored");
 
   const jsCode = await transform(
     svgCode,
-    { ...config, template: isColored ? coloredTmp : tmp },
-    { componentName: componentName, filePath: `./output/${componentName}.js` }
+    {
+      ...config,
+      template,
+    },
+    { componentName }
   );
 
   const regularPath = `${regularDir}/${componentName}.svg`;
@@ -74,15 +88,19 @@ const generateJSForSvgFile = async (componentName, svgFile, regularDir) => {
   } else {
     const regularJsCode = await transform(regularSvgCode, {
       ...config,
-      template: tmpRegular,
+      template: regularTemplate,
     });
 
+    const jsCodeLines = jsCode.split("\n");
+
+    const propDestructureLineIndex = jsCodeLines.findIndex((line) =>
+      line.includes("} = props")
+    );
     const codeSnippet = `\nif (weight === "REGULAR") {
         return ${regularJsCode.substring(1)};
       }\n`;
+    jsCodeLines.splice(propDestructureLineIndex + 1, 0, codeSnippet);
 
-    const jsCodeLines = jsCode.split("\n");
-    jsCodeLines.splice(6, 0, codeSnippet);
     const combinedJsCode = jsCodeLines.join("\n");
 
     resultJSCode = combinedJsCode;
@@ -145,69 +163,20 @@ const generateJSForSvgFile = async (componentName, svgFile, regularDir) => {
     );
   }
 
-  // if the component name contains "Filled" and resultJsCode contains word fill="white" then
-  // add stroke: PropTypes.string, to propTypes
-  // add stroke: colors.iconOn, to defaultProps
-  // destructure stroke from props
-  // replace fill="white" with fill={stroke}
-  // const isFilledIcon =
-  //   componentName.includes("Filled") && resultJSCode.includes(`fill="white"`);
-  // if (isFilledIcon) {
-  //   const lines2 = resultJSCode.split("\n");
-  //   const strokePropTypeLineIndex = lines2.findIndex(
-  //     (line) => line === `${componentName}.propTypes = {`
-  //   );
-  //   if (strokePropTypeLineIndex !== -1) {
-  //     lines2.splice(
-  //       strokePropTypeLineIndex + 1,
-  //       0,
-  //       `  stroke: PropTypes.string,`
-  //     );
-  //   }
+  await writeFile(`${outputDir}/${componentName}.js`, resultJSCode);
 
-  //   const strokeDefaultPropsLineIndex = lines2.findIndex(
-  //     (line) => line === `${componentName}.defaultProps = {`
-  //   );
-  //   if (strokeDefaultPropsLineIndex !== -1) {
-  //     lines2.splice(
-  //       strokeDefaultPropsLineIndex + 1,
-  //       0,
-  //       `  stroke: colors.iconOn,`
-  //     );
-  //   }
-
-  //   const strokeDestructureLineIndex = lines2.findIndex(
-  //     (line) => line === `const ${componentName} = (props) => {`
-  //   );
-  //   if (strokeDestructureLineIndex !== -1) {
-  //     lines2.splice(
-  //       strokeDestructureLineIndex + 1,
-  //       1,
-  //       `    const { size, weight, fill, stroke } = props`
-  //     );
-  //   }
-
-  //   resultJSCode = lines2.join("\n");
-
-  //   resultJSCode = resultJSCode.replace(/fill="white"/g, "fill={stroke}");
-  // }
-
-  await writeFile(`./output/${componentName}.js`, resultJSCode);
   return {
     hasRegularWeight,
     componentName,
     isColored,
-    // hasStrokeAndFill: isFilledIcon,
   };
 };
 
 const main = async () => {
-  const inputDir = "./input/Bold";
-  const regularDir = "./input/Regular";
-  const coloredDir = "./input/Colored";
   const allBoldFiles = await readdir(inputDir);
   const allRegularFiles = await readdir(regularDir);
   const allColoredFiles = await readdir(coloredDir);
+
   const svgBoldFiles = allBoldFiles.filter((file) => file.endsWith(".svg"));
   const svgRegularFiles = allRegularFiles.filter((file) =>
     file.endsWith(".svg")
@@ -218,27 +187,34 @@ const main = async () => {
 
   const jsCodePromises = [];
   svgBoldFiles.forEach((file) => {
-    const filePath = path.join(inputDir, file);
-    const componentName = path.parse(filePath).name;
-
+    const svgFilePath = path.join(inputDir, file);
+    const componentName = path.parse(svgFilePath).name;
     jsCodePromises.push(
-      generateJSForSvgFile(componentName, filePath, regularDir)
+      generateJSForSvgFile({
+        componentName,
+        svgFilePath,
+        template: boldTemplate,
+      })
     );
   });
 
   svgColoredFiles.forEach((file) => {
-    const filePath = path.join(coloredDir, file);
-    const componentName = path.parse(filePath).name;
-    jsCodePromises.push(generateJSForSvgFile(componentName, filePath, ""));
+    const svgFilePath = path.join(coloredDir, file);
+    const componentName = path.parse(svgFilePath).name;
+    jsCodePromises.push(
+      generateJSForSvgFile({
+        componentName,
+        svgFilePath,
+        template: coloredTemplate,
+      })
+    );
   });
 
   await Promise.all(jsCodePromises).then((values) => {
     const regularWeightIconCount = values.filter(
       (value) => value.hasRegularWeight
     ).length;
-    // const hasStrokeAndFillCount = values.filter(
-    //   (value) => value.hasStrokeAndFill
-    // ).length;
+
     const coloredIconCount = values.filter((value) => value.isColored).length;
 
     console.log("====== Input Summary ======");
@@ -250,7 +226,7 @@ const main = async () => {
     console.log(`Total icons: ${values.length}`);
     console.log(`Regular weight icon: ${regularWeightIconCount}`);
     console.log(`Colored icon: ${coloredIconCount}`);
-    // console.log(`Has Stroke and Fill both icon: ${hasStrokeAndFillCount}`);
+
     console.log("====== csv for summary ======");
     console.log(`ComponentName,HasRegularWeight,IsColored`);
     values.forEach((value) => {
