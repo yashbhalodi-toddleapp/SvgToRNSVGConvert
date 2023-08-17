3e2aee5e8392014aaca4c9c10acd47b0f0c06de4
yashbhalodi-toddleapp <yashbhalodi@toddleapp.com>
2023-08-17 13:12:00 +0530

    chore: regenerate script

--- output/UnitLibraryOutlined.js
+++ output/UnitLibraryOutlined.js
@@ -11,7 +11,7 @@ const UnitLibraryOutlined = (props) => {
       <Svg
         width={size}
         height={size}
-        viewBox="0 0 33 32"
+        viewBox="0 0 32 32"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
