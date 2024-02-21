4d6564caaa43d97559748a3e1f2754ab06deeb0b
Anubhav <116361157+anubhav-toddleapp@users.noreply.github.com>
2024-02-21 12:27:11 +0530

    chore: add regular variant for FilterThreeLinesCircleOutlined and FilterThreeLinesCircleFilled

--- output/FilterThreeLinesCircleFilled.js
+++ output/FilterThreeLinesCircleFilled.js
@@ -5,6 +5,26 @@ import { colors } from "js/Common/UIElements/colors";
 
 const FilterThreeLinesCircleFilled = (props) => {
   const { size, weight, fill } = props;
+
+  if (weight === "REGULAR") {
+    return (
+      <Svg
+        width={size}
+        height={size}
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M2 16C2 23.732 8.26801 30 16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2C8.26801 2 2 8.26801 2 16ZM11 10C10.4477 10 10 10.4477 10 11C10 11.5523 10.4477 12 11 12H21C21.5523 12 22 11.5523 22 11C22 10.4477 21.5523 10 21 10H11ZM12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17H20C20.5523 17 21 16.5523 21 16C21 15.4477 20.5523 15 20 15H12ZM13 21C13 20.4477 13.4477 20 14 20H18C18.5523 20 19 20.4477 19 21C19 21.5523 18.5523 22 18 22H14C13.4477 22 13 21.5523 13 21Z"
+          fill={fill}
+        />
+      </Svg>
+    );
+  }
+
   return (
     <Svg
       width={size}
@@ -25,7 +45,7 @@ const FilterThreeLinesCircleFilled = (props) => {
 
 FilterThreeLinesCircleFilled.propTypes = {
   size: PropTypes.oneOf([16, 20, 24, 32]),
-  weight: PropTypes.oneOf(["BOLD"]),
+  weight: PropTypes.oneOf(["REGULAR", "BOLD"]),
   fill: PropTypes.string,
 };
 
--- output/FilterThreeLinesCircleOutlined.js
+++ output/FilterThreeLinesCircleOutlined.js
@@ -5,6 +5,38 @@ import { colors } from "js/Common/UIElements/colors";
 
 const FilterThreeLinesCircleOutlined = (props) => {
   const { size, weight, fill } = props;
+
+  if (weight === "REGULAR") {
+    return (
+      <Svg
+        width={size}
+        height={size}
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <Path
+          d="M12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17H20C20.5523 17 21 16.5523 21 16C21 15.4477 20.5523 15 20 15H12Z"
+          fill={fill}
+        />
+        <Path
+          d="M10 11C10 10.4477 10.4477 10 11 10H21C21.5523 10 22 10.4477 22 11C22 11.5523 21.5523 12 21 12H11C10.4477 12 10 11.5523 10 11Z"
+          fill={fill}
+        />
+        <Path
+          d="M14 20C13.4477 20 13 20.4477 13 21C13 21.5523 13.4477 22 14 22H18C18.5523 22 19 21.5523 19 21C19 20.4477 18.5523 20 18 20H14Z"
+          fill={fill}
+        />
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M2 16C2 23.732 8.26801 30 16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2C8.26801 2 2 8.26801 2 16ZM16 28C9.37258 28 4 22.6274 4 16C4 9.37258 9.37258 4 16 4C22.6274 4 28 9.37258 28 16C28 22.6274 22.6274 28 16 28Z"
+          fill={fill}
+        />
+      </Svg>
+    );
+  }
+
   return (
     <Svg
       width={size}
@@ -37,7 +69,7 @@ const FilterThreeLinesCircleOutlined = (props) => {
 
 FilterThreeLinesCircleOutlined.propTypes = {
   size: PropTypes.oneOf([16, 20, 24, 32]),
-  weight: PropTypes.oneOf(["BOLD"]),
+  weight: PropTypes.oneOf(["REGULAR", "BOLD"]),
   fill: PropTypes.string,
 };
 
